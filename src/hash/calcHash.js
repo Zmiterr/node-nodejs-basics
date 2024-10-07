3288bf0c666f88ce9ec9a890811c420326fc4418
D.Rabinkin <dima.ryabinkin@dfmx.ae>
2024-10-07 15:35:18 +0300

    feat: Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        await fs.promises.access(filePath);
+
+        const hash = crypto.createHash('sha256');
+        const stream = fs.createReadStream(filePath);
+
+        stream.on('data', chunk => hash.update(chunk));
+        stream.on('end', () => {
+            console.log(hash.digest('hex'));
+        });
+
+        stream.on('error', (error) => {
+            console.error('FS operation failed:', error);
+        });
+    } catch (error) {
+        console.error('FS operation failed:', error);
+    }
 };
 
-await calculateHash();
+await calculateHash(); // Вызов функции
